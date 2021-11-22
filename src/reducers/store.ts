ae5df5086e87b5292ab5f2100af6c127ad373e5e
saseungmin <dbd02169@naver.com>
2021-11-23 02:08:22 +0900

    WIP

--- src/reducers/store.ts
+++ src/reducers/store.ts
@@ -1,20 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 
 import { isProdLevel } from '@/utils/utils';
 
 import rootReducer from './rootReducer';
 
-export const makeStore = configureStore({
+export const makeStore = () => configureStore({
   reducer: rootReducer,
   devTools: !isProdLevel(process.env.NODE_ENV),
 });
 
-const wrapper = createWrapper(() => makeStore, {
+const wrapper = createWrapper(makeStore, {
   debug: !isProdLevel(process.env.NODE_ENV),
 });
 
-export type AppDispatch = typeof makeStore.dispatch
-export type RootState = ReturnType<typeof makeStore.getState>
+const store = makeStore();
 
+export type AppState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action<string>
+>
 export default wrapper;
