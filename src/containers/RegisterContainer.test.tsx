8dcadbeba8e59103adc45a80c4b2883bdcb071fd
Harang <dbd02169@naver.com>
2021-11-28 01:00:24 +0900

    [Feature] 로그인 후 기본 회원 정보를 등록하는 Form을 구현하라 (#36)
    - Install dependency react-hook-form, @hookform/resolvers, yup
    - 이름, 이메일(고정), 포트폴리오 URL(option), 아이디 form
    - 이름 빈칸일 때: "이름을 입력해주세요."
    - 아이디 빈칸일 때: "아이디를 입력해주세요."

--- src/containers/RegisterContainer.test.tsx
+++ src/containers/RegisterContainer.test.tsx
@@ -1,15 +1,69 @@
-import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+
+import {
+  act, fireEvent, render, screen,
+} from '@testing-library/react';
+
+import PROFILE_FIXTURE from '../../fixtures/profile';
 
 import RegisterContainer from './RegisterContainer';
 
 describe('RegisterContainer', () => {
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockImplementation((selector) => selector({
+      authReducer: {
+        auth: given.auth,
+      },
+    }));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   const renderRegisterContainer = () => render((
     <RegisterContainer />
   ));
 
-  it('회원가입 페이지에 대한 정보 나타나야만 한다', () => {
-    const { container } = renderRegisterContainer();
+  context('로그인 후 가입페이지로 redirect된 경우', () => {
+    given('auth', () => (PROFILE_FIXTURE));
 
-    expect(container).toHaveTextContent('기본 회원 정보를 등록해주세요.');
+    it('회원가입 페이지에 대한 정보 나타나야만 한다', () => {
+      const { container } = renderRegisterContainer();
+
+      expect(container).toHaveTextContent('기본 회원 정보를 등록해주세요.');
+    });
+
+    describe('저장버튼을 클리한다', () => {
+      const spyOnConsole = jest.spyOn(window.console, 'log');
+
+      beforeEach(() => {
+        spyOnConsole.mockReturnValueOnce();
+      });
+
+      it('"console.log"가 호출되어야만 한다', async () => {
+        renderRegisterContainer();
+
+        await act(async () => {
+          await fireEvent.submit(screen.getByText('저장'));
+        });
+
+        expect(spyOnConsole).toBeCalledWith({
+          displayName: 'test',
+          id: 'test',
+          portfolioUrl: '',
+        });
+      });
+    });
+  });
+
+  context('비정상적으로 가입페이지로 redirect된 경우', () => {
+    given('auth', () => (null));
+
+    it('"로그인부터 진행해주세요!" 메시지가 나타나야만 한다', () => {
+      const { container } = renderRegisterContainer();
+
+      expect(container).toHaveTextContent('로그인부터 진행해주세요!');
+    });
   });
 });
