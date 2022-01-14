88c5447deb20311f0034b82b7eae73405b8ef5fd
jooseyoung <aa.jooseyoung@gmail.com>
2022-01-14 23:00:17 +0900

    [uiux] 텍스트 스타일의 단위를 px에서 rem으로 변경하라
    - 텍스트 스타일의 단위를 px에서 rem으로 변경
    - RecruitPost에 배경색을 적용

--- src/styles/fontStyles.ts
+++ src/styles/fontStyles.ts
@@ -6,36 +6,36 @@ const fontWeight = (isBold: boolean) => css`
 
 export const h2Font = (isBold = false) => css`
   font-weight: ${isBold ? 'bold' : 'normal'};
-  font-size: 32px;
-  line-height: 48px;
+  font-size: 2rem;
+  line-height: 3rem;
 `;
 
 export const h3Font = (isBold = false) => css`
   ${fontWeight(isBold)};
-  font-size: 22px;
-  line-height: 32px;
+  font-size: 1.375rem;
+  line-height: 2rem;
 `;
 
 export const h4Font = (isBold = false) => css`
   ${fontWeight(isBold)};
-  font-size: 17px;
-  line-height: 24px;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
 `;
 
 export const body1Font = (isBold = false) => css`
   ${fontWeight(isBold)};
-  font-size: 15px;
-  line-height: 24px;
+  font-size: 1rem;
+  line-height: 1.625rem;
 `;
 
 export const body2Font = (isBold = false) => css`
   ${fontWeight(isBold)};
-  font-size: 14px;
-  line-height: 22px;
+  font-size: 0.875rem;
+  line-height: 1.375rem;
 `;
 
 export const subtitle1Font = (isBold = false) => css`
   ${fontWeight(isBold)};
-  font-size: 13px;
-  line-height: 20px;
+  font-size: 0.825rem;
+  line-height: 1.25rem;
 `;
