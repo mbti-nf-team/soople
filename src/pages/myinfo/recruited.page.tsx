345ccd3d9a7534168d7af6d0fbef08bd73b0d020
Jennie <97030008+jooseyoung@users.noreply.github.com>
2022-01-27 00:29:22 +0900

    [UI/UX] myinfo 페이지의 탭 스타일과 Empty State를 구현 (#196)
    * [UI/UX] myinfo 페이지의 탭 스타일과 Empty State를 구현
    
    - myinfo 상단의 MyInfoTab 스타일 적용
    - 신청한 팀, 모집한 팀 Empty State 구현
    
    * [Refactor] 깨지는 테스트 케이스를 수정하라
    
    - MyInfoTab 테스트 코드 수정
    - CSS position sticky 적용
    
    Co-authored-by: saseungmin <dbd02169@naver.com>

--- src/pages/myinfo/recruited.page.tsx
+++ src/pages/myinfo/recruited.page.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 
 import { GetServerSideProps } from 'next';
 
+import EmptyStateArea from '@/components/common/EmptyStateArea';
 import getMyInfoLayout from '@/components/myInfo/MyInfoLayout';
 import authenticatedServerSideProps from '@/services/serverSideProps/authenticatedServerSideProps';
 
@@ -9,7 +10,12 @@ export const getServerSideProps: GetServerSideProps = authenticatedServerSidePro
 
 function RecruitedPage(): ReactElement {
   return (
-    <div>모집한 팀</div>
+    <EmptyStateArea
+      emptyText="모집한 팀이 없어요."
+      buttonText="팀 모집하기"
+      href="/write"
+      marginTop="80px"
+    />
   );
 }
 
