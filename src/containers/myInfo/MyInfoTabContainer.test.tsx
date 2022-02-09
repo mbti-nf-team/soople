72d24fd421989644c1bc014cfd8bcdc29915974c
saseungmin <dbd02169@naver.com>
2022-02-09 11:57:07 +0900

    [Refactor] 테스트 코드를 추가하라
    - 누락 테스트 코드 추가

--- src/containers/myInfo/MyInfoTabContainer.test.tsx
+++ src/containers/myInfo/MyInfoTabContainer.test.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react';
 
+import InjectMockProviders from '@/test/InjectMockProviders';
+
 import MyInfoTabContainer from './MyInfoTabContainer';
 
 describe('MyInfoTabContainer', () => {
   const renderMyInfoTabContainer = () => render((
-    <MyInfoTabContainer activeTab="setting" />
+    <InjectMockProviders>
+      <MyInfoTabContainer activeTab="setting" />
+    </InjectMockProviders>
   ));
 
   it('내 정보 탭에 대한 내용이 나타나야만 한다', () => {
