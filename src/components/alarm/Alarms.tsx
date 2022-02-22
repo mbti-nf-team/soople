5f8b5075deddce57624869170f99da4728cf03a4
jooseyoung <aa.jooseyoung@gmail.com>
2022-02-22 20:16:56 +0900

    WIP

--- src/components/alarm/Alarms.tsx
+++ src/components/alarm/Alarms.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from 'react';
 
 import { Alarm } from '@/models/alarm';
 
+import EmptyStateArea from '../common/EmptyStateArea';
+
 import AlarmItem from './AlarmItem';
 
 interface Props {
@@ -12,6 +14,17 @@ interface Props {
 function Alarms({ alarms, isLoading }: Props): ReactElement {
   if (isLoading) {
     return <>로딩중...</>;
+  }
+
+  if (alarms.length === 0) {
+    return (
+      <EmptyStateArea
+        emptyText="알림이 없어요."
+        buttonText="팀 살펴보기"
+        href="/"
+        marginTop="80px"
+      />
+    );
   }
 
   return (
