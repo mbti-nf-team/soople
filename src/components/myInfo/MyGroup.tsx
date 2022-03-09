1346da0735bfb893d48a535685a743d8b88c27a7
Harang <dbd02169@naver.com>
2022-03-09 21:47:12 +0900

    [Feature] 마이페이지의 팀 리스트에 썸네일과 짧은 소개글을 볼 수 있게하라 (#305)
    - 마이페이지에 썸네일과 짧은 소개글 적용

--- src/components/myInfo/MyGroup.tsx
+++ src/components/myInfo/MyGroup.tsx
@@ -29,7 +29,8 @@ interface Props {
 
 function MyGroup({ group, onClick }: Props): ReactElement {
   const {
-    title, content, createdAt, groupId, numberApplicants, recruitmentEndDate, isCompleted,
+    title, content, createdAt, groupId, numberApplicants,
+    recruitmentEndDate, isCompleted, thumbnail, shortDescription,
   } = group;
   const currentTime = useCurrentTime(group);
   const status = useGroupRecruitmentStatus(group);
@@ -54,19 +55,24 @@ function MyGroup({ group, onClick }: Props): ReactElement {
       onClick={() => onClick(groupId)}
       tabIndex={0}
     >
-      <MyGroupContents>
-        <div className="group-title">{title}</div>
-        <div className="group-content">
-          {removeAllHtml(content)}
-        </div>
-      </MyGroupContents>
-      <GroupMetaData status={status}>
-        <div className="date-status" data-testid="date-status">{dateStatus}</div>
-        <Divider />
-        <div className="number-applied">{numberApplied}</div>
-        <Divider />
-        <div>{dayjs(createdAt).format('YYYY년 MM월 DD일')}</div>
-      </GroupMetaData>
+      <div>
+        <MyGroupContents>
+          <div className="group-title">{title}</div>
+          <div className="group-content">
+            {shortDescription || removeAllHtml(content)}
+          </div>
+        </MyGroupContents>
+        <GroupMetaData status={status}>
+          <div className="date-status" data-testid="date-status">{dateStatus}</div>
+          <Divider />
+          <div className="number-applied">{numberApplied}</div>
+          <Divider />
+          <div>{dayjs(createdAt).format('YYYY년 MM월 DD일')}</div>
+        </GroupMetaData>
+      </div>
+      {thumbnail && (
+        <Thumbnail src={thumbnail} alt="thumbnail" />
+      )}
     </MyGroupWrapper>
   );
 }
@@ -75,6 +81,11 @@ export default MyGroup;
 
 const MyGroupWrapper = styled.div`
   cursor: pointer;
+  outline: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
 `;
 
 const MyGroupContents = styled.div`
@@ -88,6 +99,13 @@ const MyGroupContents = styled.div`
   .group-content {
     ${body1Font()};
     color: ${palette.accent7};
+    word-break: break-all;
+    overflow-wrap: break-word;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
   }
 `;
 
@@ -110,4 +128,11 @@ const GroupMetaData = styled.div<{ status: RecruitmentStatus; }>`
   .number-applied {
     color: ${palette.foreground};
   }
+`;
+
+const Thumbnail = styled.img`
+  width: 174px;
+  height: 96px;
+  border-radius: 8px;
+  margin-left: 24px;
 `;
