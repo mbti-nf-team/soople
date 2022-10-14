aeb2c6b35b1d1fab52c6e30d43969a045ee9e911
saseungmin <dbd02169@naver.com>
2022-10-14 23:13:22 +0900

    feat: 내 정보 수정에 이미지 변경 로직을 구현하라
    - 이미지 변경 버튼 클릭시 사용자 프로필 이미지 변경
    - 이미지가 이미 존재한 경우에는 기존의 이미지 삭제 후 새로운 이미지 업로드

--- src/components/myInfo/ImageSetting.test.tsx
+++ src/components/myInfo/ImageSetting.test.tsx
@@ -1,16 +1,23 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import {
+  act, fireEvent, render, screen,
+} from '@testing-library/react';
 
 import ImageSetting from './ImageSetting';
 
 describe('ImageSetting', () => {
   const handleDelete = jest.fn();
+  const handleUpload = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   const renderImageSetting = () => render((
-    <ImageSetting imageUrl="/test" onDelete={handleDelete} />
+    <ImageSetting
+      imageUrl="/test"
+      onUpload={handleUpload}
+      onDelete={handleDelete}
+    />
   ));
 
   it('내 이미지에 대한 정보가 나타나야만 한다', () => {
@@ -27,6 +34,42 @@ describe('ImageSetting', () => {
       fireEvent.click(screen.getByText('이미지 삭제'));
 
       expect(handleDelete).toBeCalledTimes(1);
+    });
+  });
+
+  describe('"이미지 선택" 버튼을 클릭한다', () => {
+    context('이미지 파일이 추가되지 않았을 경우', () => {
+      it('업로드 이벤트가 호출되지 않아야만 한다', () => {
+        renderImageSetting();
+
+        fireEvent.click(screen.getByText('이미지 선택'));
+
+        act(() => {
+          fireEvent.change(screen.getByTestId('upload-profile-image-input'), {
+            target: { files: [] },
+          });
+        });
+
+        expect(handleUpload).not.toBeCalled();
+      });
+    });
+
+    context('이미지 파일이 추가된 경우', () => {
+      const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
+
+      it('업로드 이벤트가 호출되어야만 한다', () => {
+        renderImageSetting();
+
+        fireEvent.click(screen.getByText('이미지 선택'));
+
+        act(() => {
+          fireEvent.change(screen.getByTestId('upload-profile-image-input'), {
+            target: { files: [file] },
+          });
+        });
+
+        expect(handleUpload).toBeCalledTimes(1);
+      });
     });
   });
 });
--- src/components/myInfo/ImageSetting.tsx
+++ src/components/myInfo/ImageSetting.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement, useRef } from 'react';
 
 import styled from '@emotion/styled';
 
@@ -8,18 +8,40 @@ import ProfileImage from '../common/ProfileImage';
 interface Props {
   imageUrl?: string | null;
   onDelete: () => void;
+  onUpload: (image: File) => void;
 }
 
-function ImageSetting({ imageUrl, onDelete }: Props):ReactElement {
+function ImageSetting({ imageUrl, onDelete, onUpload }: Props):ReactElement {
+  const fileUploadInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickSelectImage = () => fileUploadInputRef.current?.click();
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length) {
+      return;
+    }
+
+    onUpload(e.target.files[0]);
+  };
+
   return (
     <ImageSettingWrapper>
       <ProfileImage src={imageUrl} size="96px" alt="프로필 이미지" />
       <Button
         size="small"
         color="primary"
+        onClick={handleClickSelectImage}
       >
         이미지 선택
       </Button>
+      <input
+        type="file"
+        style={{ display: 'none' }}
+        onChange={handleChangeImage}
+        data-testid="upload-profile-image-input"
+        accept="image/png, image/jpeg, image/jpg"
+        ref={fileUploadInputRef}
+      />
       <Button
         size="small"
         color="ghost"
