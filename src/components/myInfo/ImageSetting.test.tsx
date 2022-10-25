74bd6ec1d6a375f0296fc7e69bc19cc40e7f20f5
saseungmin <dbd02169@naver.com>
2022-10-25 23:27:13 +0900

    feat: 내 정보 수정 페이지의 이미지 등록시 max size 및 허용 이미지 확장자를 추가하라
    - 내 정보 수정 > 이미지 등록시 max size 및 허용 이미지 확장자를 추가
      - 5MB
      - PNG, JPG, JPEG, GIF

--- src/components/myInfo/ImageSetting.test.tsx
+++ src/components/myInfo/ImageSetting.test.tsx
@@ -2,7 +2,11 @@ import {
   act, fireEvent, render, screen,
 } from '@testing-library/react';
 
+import { errorToast } from '@/utils/toast';
+
 import ImageSetting from './ImageSetting';
+
+jest.mock('@/utils/toast');
 
 describe('ImageSetting', () => {
   const handleDelete = jest.fn();
@@ -55,20 +59,41 @@ describe('ImageSetting', () => {
     });
 
     context('이미지 파일이 추가된 경우', () => {
-      const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
+      context('이미지 사이즈가 5MB보다 작은 경우', () => {
+        const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
 
-      it('업로드 이벤트가 호출되어야만 한다', () => {
-        renderImageSetting();
+        it('업로드 이벤트가 호출되어야만 한다', () => {
+          renderImageSetting();
 
-        fireEvent.click(screen.getByText('이미지 선택'));
+          fireEvent.click(screen.getByText('이미지 선택'));
 
-        act(() => {
-          fireEvent.change(screen.getByTestId('upload-profile-image-input'), {
-            target: { files: [file] },
+          act(() => {
+            fireEvent.change(screen.getByTestId('upload-profile-image-input'), {
+              target: { files: [file] },
+            });
           });
-        });
 
-        expect(handleUpload).toBeCalledTimes(1);
+          expect(handleUpload).toBeCalledTimes(1);
+        });
+      });
+
+      context('이미지 사이즈가 5MB보다 큰 경우', () => {
+        const file = new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' });
+        Object.defineProperty(file, 'size', { value: 100000000 });
+
+        it('에러 토스트 메시지가 "이미지 업로드 최대 사이즈는 5MB입니다."와 함께 호출되어야만 한다', () => {
+          renderImageSetting();
+
+          fireEvent.click(screen.getByText('이미지 선택'));
+
+          act(() => {
+            fireEvent.change(screen.getByTestId('upload-profile-image-input'), {
+              target: { files: [file] },
+            });
+          });
+
+          expect(errorToast).toBeCalledWith('이미지 업로드 최대 사이즈는 5MB입니다.');
+        });
       });
     });
   });
