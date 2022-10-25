978085a88a6e6f87d85ca1cfe304fde141a07a79
Harang <dbd02169@naver.com>
2022-10-25 23:32:42 +0900

    feat: 내 정보 수정 페이지의 이미지 등록시 max size 및 허용 이미지 확장자를 추가하라 (#558)
    - 내 정보 수정 > 이미지 등록시 max size 및 허용 이미지 확장자를 추가
      - 5MB
      - PNG, JPG, JPEG, GIF

--- src/components/myInfo/ImageSetting.tsx
+++ src/components/myInfo/ImageSetting.tsx
@@ -4,6 +4,8 @@ import React, {
 
 import styled from '@emotion/styled';
 
+import { errorToast } from '@/utils/toast';
+
 import Button from '../common/Button';
 import ProfileImage from '../common/ProfileImage';
 
@@ -13,6 +15,8 @@ interface Props {
   onUpload: (image: File) => void;
 }
 
+const IMAGE_MAX_SIZE = 5242880;
+
 function ImageSetting({ imageUrl, onDelete, onUpload }: Props):ReactElement {
   const fileUploadInputRef = useRef<HTMLInputElement>(null);
 
@@ -20,6 +24,11 @@ function ImageSetting({ imageUrl, onDelete, onUpload }: Props):ReactElement {
 
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files?.length) {
+      return;
+    }
+
+    if (e.target.files[0].size > IMAGE_MAX_SIZE) {
+      errorToast('이미지 업로드 최대 사이즈는 5MB입니다.');
       return;
     }
 
@@ -41,7 +50,7 @@ function ImageSetting({ imageUrl, onDelete, onUpload }: Props):ReactElement {
         style={{ display: 'none' }}
         onChange={handleChangeImage}
         data-testid="upload-profile-image-input"
-        accept="image/png, image/jpeg, image/jpg"
+        accept="image/png, image/jpeg, image/jpg, image/gif"
         ref={fileUploadInputRef}
       />
       <Button
