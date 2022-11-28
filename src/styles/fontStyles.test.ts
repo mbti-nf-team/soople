94b49ab999c6f58b453b5e46ecdd71a43547a4b9
saseungmin <dbd02169@naver.com>
2022-11-28 22:39:27 +0900

    docs: 스토리북에 typography를 추가하라
    - 스토리북 typography를 추가하라

--- src/styles/fontStyles.test.ts
+++ src/styles/fontStyles.test.ts
@@ -57,41 +57,91 @@ describe('h3Font', () => {
 });
 
 describe('h4Font', () => {
-  it('font-size는 1.125rem이어야만 한다', () => {
-    const result = h4Font();
+  context('true인 경우', () => {
+    it('"font-weight" 속성은 600여야만 한다', () => {
+      const result = h4Font(true);
 
-    expect(result.styles).toContain('font-size:1.125rem;');
+      expect(result.styles).toContain('font-weight:600;');
+    });
+  });
+
+  context('false인 경우', () => {
+    it('"font-weight" 속성은 normal여야만 한다', () => {
+      const result = h4Font();
+
+      expect(result.styles).toContain('font-weight:normal;');
+    });
   });
 });
 
 describe('body1Font', () => {
-  it('font-size는 1rem이어야만 한다', () => {
-    const result = body1Font();
+  context('true인 경우', () => {
+    it('"font-weight" 속성은 600여야만 한다', () => {
+      const result = body1Font(true);
 
-    expect(result.styles).toContain('font-size:1rem;');
+      expect(result.styles).toContain('font-weight:600;');
+    });
+  });
+
+  context('false인 경우', () => {
+    it('"font-weight" 속성은 normal여야만 한다', () => {
+      const result = body1Font();
+
+      expect(result.styles).toContain('font-weight:normal;');
+    });
   });
 });
 
 describe('body2Font', () => {
-  it('font-size는 0.875rem이어야만 한다', () => {
-    const result = body2Font();
+  context('true인 경우', () => {
+    it('"font-weight" 속성은 600여야만 한다', () => {
+      const result = body2Font(true);
 
-    expect(result.styles).toContain('font-size:0.875rem;');
+      expect(result.styles).toContain('font-weight:600;');
+    });
+  });
+
+  context('false인 경우', () => {
+    it('"font-weight" 속성은 normal여야만 한다', () => {
+      const result = body2Font();
+
+      expect(result.styles).toContain('font-weight:normal;');
+    });
   });
 });
 
 describe('subtitle1Font', () => {
-  it('font-size는 0.825rem이어야만 한다', () => {
-    const result = subtitle1Font();
+  context('true인 경우', () => {
+    it('"font-weight" 속성은 600여야만 한다', () => {
+      const result = subtitle1Font(true);
 
-    expect(result.styles).toContain('font-size:0.825rem;');
+      expect(result.styles).toContain('font-weight:600;');
+    });
+  });
+
+  context('false인 경우', () => {
+    it('"font-weight" 속성은 normal여야만 한다', () => {
+      const result = subtitle1Font();
+
+      expect(result.styles).toContain('font-weight:normal;');
+    });
   });
 });
 
 describe('subtitle2Font', () => {
-  it('font-size는 0.7rem이어야만 한다', () => {
-    const result = subtitle2Font();
+  context('true인 경우', () => {
+    it('"font-weight" 속성은 600여야만 한다', () => {
+      const result = subtitle2Font(true);
 
-    expect(result.styles).toContain('font-size:0.7rem;');
+      expect(result.styles).toContain('font-weight:600;');
+    });
+  });
+
+  context('false인 경우', () => {
+    it('"font-weight" 속성은 normal여야만 한다', () => {
+      const result = subtitle2Font();
+
+      expect(result.styles).toContain('font-weight:normal;');
+    });
   });
 });
