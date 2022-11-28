7883c48bc1cd72524a1d25b957a84cca64f8681a
Harang <dbd02169@naver.com>
2022-11-28 22:47:52 +0900

    docs: 스토리북에 typography를 추가하라 (#579)
    - 스토리북 typography를 추가

--- src/styles/fontStyles.ts
+++ src/styles/fontStyles.ts
@@ -1,9 +1,5 @@
 import { css } from '@emotion/react';
 
-const fontWeight = (isBold: boolean) => css`
-  font-weight: ${isBold ? '600' : 'normal'};
-`;
-
 export const h1Font = (isBold = false) => css`
   font-weight: ${isBold ? 'bold' : 'normal'};
   font-size: 2.5rem;
@@ -17,37 +13,37 @@ export const h2Font = (isBold = false) => css`
 `;
 
 export const h3Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 1.375rem;
   line-height: 2rem;
 `;
 
 export const h4Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 1.125rem;
   line-height: 1.75rem;
 `;
 
 export const body1Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 1rem;
   line-height: 1.625rem;
 `;
 
 export const body2Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 0.875rem;
   line-height: 1.375rem;
 `;
 
 export const subtitle1Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 0.825rem;
   line-height: 1.25rem;
 `;
 
 export const subtitle2Font = (isBold = false) => css`
-  ${fontWeight(isBold)};
+  font-weight: ${isBold ? '600' : 'normal'};
   font-size: 0.7rem;
   line-height: 1rem;
 `;
