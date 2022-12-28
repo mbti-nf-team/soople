89c9ba13ccbe7a8650321e50ead91ea0eef89986
saseungmin <dbd02169@naver.com>
2022-12-28 22:51:42 +0900

    feat(ErrorBoundary): errorBoundary 공통 컴포넌트를 구현하라
    - ErrorBoundary 공통 컴포넌트를 구현
    - Sentry ErrorBoundary 사용

---
+++ src/components/common/ErrorBoundary.test.tsx
@@ -0,0 +1,69 @@
+import { useRouter } from 'next/router';
+
+import { render } from '@testing-library/react';
+
+import ReactQueryWrapper from '../../test/ReactQueryWrapper';
+
+import ErrorBoundary from './ErrorBoundary';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
+describe('ErrorBoundary', () => {
+  const errorMessage = '에러가 발생했어요.';
+  const mockReplace = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    jest.spyOn(console, 'error').mockImplementation(jest.fn);
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      replace: mockReplace,
+    }));
+  });
+
+  const MockErrorComponent = () => {
+    throw new Error('test');
+  };
+
+  const renderErrorBoundary = () => render(
+    <ReactQueryWrapper>
+      <ErrorBoundary isRootError={given.isRootError} errorMessage={errorMessage}>
+        <MockErrorComponent />
+      </ErrorBoundary>
+    </ReactQueryWrapper>,
+  );
+
+  context('isRootError가 true인 경우', () => {
+    given('isRootError', () => true);
+
+    it('에러 메시지가 보여야만 한다', () => {
+      const { container } = renderErrorBoundary();
+
+      expect(container).toHaveTextContent(errorMessage);
+    });
+
+    it('router.replace가 "/500"과 함께 호출되어야만 한다', () => {
+      renderErrorBoundary();
+
+      expect(mockReplace).toBeCalledWith('/500');
+    });
+  });
+
+  context('isRootError가 false인 경우', () => {
+    given('isRootError', () => false);
+
+    it('에러 메시지가 보여야만 한다', () => {
+      const { container } = renderErrorBoundary();
+
+      expect(container).toHaveTextContent(errorMessage);
+    });
+
+    it('router.replace는 호출되지 않아만한다', () => {
+      renderErrorBoundary();
+
+      expect(mockReplace).not.toBeCalled();
+    });
+  });
+});
---
+++ src/components/common/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren, useCallback } from 'react';
+
+import { useRouter } from 'next/router';
+
+import { ErrorBoundary as SentryErrorBoundary } from '@sentry/nextjs';
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+
+type Props = {
+  errorMessage: string;
+  isRootError?: boolean;
+};
+
+function ErrorBoundary({ isRootError, errorMessage, children }: PropsWithChildren<Props>) {
+  const { replace } = useRouter();
+
+  const memoizedFallbackComponent = useCallback((test: SentryFallbackRenderProps) => {
+    if (isRootError) {
+      replace('/500');
+    }
+
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <div>{errorMessage}</div>
+        <button type="button" onClick={test.resetError}>
+          재시도
+        </button>
+      </div>
+    );
+  }, [replace, isRootError, errorMessage]);
+
+  return (
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <SentryErrorBoundary
+          onReset={reset}
+          fallback={memoizedFallbackComponent}
+        >
+          {children}
+        </SentryErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
+  );
+}
+
+export default ErrorBoundary;
